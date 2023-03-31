5908aaa6238c5c5383d48bcc0ec2c7a6fa2ae44e
saurabbh1120 <126014591+saurabh8081@users.noreply.github.com>
2023-03-31 18:45:40 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return <Router>
-    <Header/>
+    <Header />
     <Routes>
       <Route path="/" element={<Home/>} />
     </Routes>
+    <Footer />
   </Router>
 }
 
